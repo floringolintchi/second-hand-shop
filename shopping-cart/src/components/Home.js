f14baca2d0a0a7e9dd530d090e18c9633f6abe39
Mihai Borcan <mihaiborcan@yahoo.com>
2020-09-07 22:41:19 +0200

    Uniform styling for navbar

--- shopping-cart/src/components/Home.js
+++ shopping-cart/src/components/Home.js
@@ -1,52 +1,58 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { addToCart } from './actions/cartActions'
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
+import {addToCart} from './actions/cartActions'
 
- class Home extends Component{
-    
-    handleClick = (id)=>{
-        this.props.addToCart(id); 
-    }
+class Home extends Component {
 
-    render(){
-        let itemList = this.props.items.map(item=>{
-            return(
-                <div className="card" key={item.id}>
-                        <div className="card-image">
-                            <img src={item.img} alt={item.title}/>
-                            <span className="card-title">{item.title}</span>
-                            <span to="/" className="btn-floating halfway-fab waves-effect waves-light red" onClick={()=>{this.handleClick(item.id)}}><i className="material-icons">add</i></span>
-                        </div>
+  handleClick = (id) => {
+    this.props.addToCart(id);
+  }
 
-                        <div className="card-content">
-                            <p>{item.desc}</p>
-                            <p><b>Price: {item.price} lei</b></p>
-                        </div>
-                 </div>
-
-            )
-        })
-
-        return(
-            <div className="container">
-                <h3 className="center">Stocul nostru</h3>
-                <div className="box">
-                    {itemList}
-                </div>
+  render() {
+    let itemList = this.props.items.map(item => {
+      return (
+          <div className="card" key={item.id}>
+            <div className="card-image">
+              <img src={item.img} alt={item.title}/>
+              <span to="/"
+                    className="btn-floating halfway-fab waves-effect waves-light red"
+                    onClick={() => {
+                      this.handleClick(item.id)
+                    }}><i className="material-icons">add</i></span>
             </div>
-        )
-    }
+            <div className="card-content">
+              <span className="card-title">{item.title}</span>
+              <p>{item.desc}</p>
+              <p><b>Price: {item.price} lei</b></p>
+            </div>
+          </div>
+
+      )
+    })
+
+    return (
+        <div className="container">
+          <h3 className="center">Stocul nostru</h3>
+          <div className="box">
+            {itemList}
+          </div>
+        </div>
+    )
+  }
 }
-const mapStateToProps = (state)=>{
-    return {
-      items: state.items
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.items
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+
+  return {
+    addToCart: (id) => {
+      dispatch(addToCart(id))
     }
   }
-const mapDispatchToProps= (dispatch)=>{
-    
-    return{
-        addToCart: (id)=>{dispatch(addToCart(id))}
-    }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
--- shopping-cart/src/components/Navbar.js
+++ shopping-cart/src/components/Navbar.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 const Navbar = ()=>{
     return(
-        <nav className="nav-wrapper">
+        <nav className="w3-bar w3-black">
             <div className="container" style={{backgroundColor:"black"}}>
-                <Link to="/" className="brand-logo">Cumpărături</Link>
+                <Link to="/" className="w3-button w3-bar-item">Cumpărături</Link>
 
                 <ul className="right">
-                    <li><Link to="/">Magazin</Link></li>
-                    <li><Link to="/cart">Coșul meu</Link></li>
-                    <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
+                    <li><Link to="/" className="w3-button w3-bar-item">Magazin</Link></li>
+                    <li><Link to="/cart" className="w3-button w3-bar-item">Coșul meu</Link></li>
                 </ul>
             </div>
         </nav>
