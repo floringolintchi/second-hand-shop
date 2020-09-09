e786a13a3641d15556f11e09bf2efd1b50710593
Florin <costacheflorinalin@gmail.com>
2020-09-09 11:00:57 +0300

    added a basic login when entering magazin

--- src/main/java/com/costache/shop/config/SecurityConfig.java
+++ src/main/java/com/costache/shop/config/SecurityConfig.java
@@ -1,35 +1,36 @@
-//package com.costache.shop.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//@Configuration
+package com.costache.shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
 //@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception
-//    {
-//       http
-//               .authorizeRequests()
-//                                    .antMatchers("/","/home").permitAll()
-//                                    .anyRequest().authenticated()
-//                                    .and()
-//               .formLogin()
-//                                    .loginPage("/login")
-//                                    .permitAll()
-//                                    .and()
-//               .logout()
-//                                    .permitAll();
-//    }
-//
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+       http
+               .authorizeRequests()
+                                    .antMatchers("/","/home","/contact","/ghiduri", "/public/static/**", "/public/img/**","/templates/**").permitAll()
+                                    .anyRequest().authenticated()
+                                    .and()
+//                                    .httpBasic();
+               .formLogin()
+//                                    .loginPage()
+                                    .permitAll()
+                                    .and()
+               .logout()
+                                    .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/img/**");
+    }
+
 //    @Bean
 //    @Override
 //    public UserDetailsService userDetailsService() {
@@ -37,12 +38,12 @@
 //        UserDetails user =
 //                User.withDefaultPasswordEncoder()
 //                    .username("user")
-//                    .password("password")
+//                    .password("123")
 //                    .roles("USER")
 //                    .build();
 //
 //        return new InMemoryUserDetailsManager(user);
 //
 //    }
-//
-//}
+
+}
